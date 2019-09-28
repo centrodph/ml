cc5c88c2a1c2c54097bf3c066e6f44e7fee048eb
Gerardo Perrucci <centrodph@gmail.com>
2019-09-28 13:28:55 -0300

    data as categories

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/importData.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/importData.py
@@ -13,10 +13,17 @@ y = dataset.iloc[:, 3].values
 
 
 # replace missing variables
-from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import LabelEncoder
 imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
 # fit the right columns 1 it's included, 3 is excluded => we sent 1 and 2
 imputer = imputer.fit(X[:, 1:3])
 
 #replace missing values with imputer
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+
+# categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
