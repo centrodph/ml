e9c3ab332ba42a71180afe8e1f9f2370ffd99e36
Gerardo Perrucci <centrodph@gmail.com>
2019-09-28 08:04:12 -0300

    select column from data set

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/importData.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/importData.py
@@ -1,3 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# import data set
+dataset = pd.read_csv('Data.csv')
+
+# take all the columns (independent variables) except the last one
+X = dataset.iloc[:, :-1].values
+
+# 3 is the index dependent variables
+y = dataset.iloc[:, 3].values
