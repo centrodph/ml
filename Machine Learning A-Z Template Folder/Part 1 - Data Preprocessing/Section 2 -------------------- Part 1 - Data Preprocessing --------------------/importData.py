6ac672c8e39c16900dc688f531b82e37c59fed84
Gerardo Perrucci <centrodph@gmail.com>
2019-09-28 08:25:27 -0300

    replace missing data python

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/importData.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/importData.py
@@ -10,3 +10,13 @@ X = dataset.iloc[:, :-1].values
 
 # 3 is the index dependent variables
 y = dataset.iloc[:, 3].values
+
+
+# replace missing variables
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
+# fit the right columns 1 it's included, 3 is excluded => we sent 1 and 2
+imputer = imputer.fit(X[:, 1:3])
+
+#replace missing values with imputer
+X[:, 1:3] = imputer.transform(X[:, 1:3])
